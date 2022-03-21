41a4104ee09a6bf4c44b08caf763831813bae8f0
Kim Vallée <kim.vallee@polytechnique.edu>
2022-03-21 17:53:09 +0100

    Trying to go to H representation, few package installation

--- src/utils.py
+++ src/utils.py
@@ -12,7 +12,7 @@
 
 """ Set of utilitary functions and constants used across the project """
 import itertools
-from typing import List, Optional, Dict
+from typing import List, Optional, Dict, Any
 
 import numpy as np
 
@@ -20,6 +20,8 @@ from src.empirical_model import EmpiricalModel
 from src.measurement_scenario import MeasurementScenario
 
 import cvxpy as cp
+from polytope.quickhull import quickhull as _quickhull
+import sympy
 
 # --------------------------------
 # Well known empirical models
@@ -52,13 +54,14 @@ EMPIRICAL_MODELS = {
 }
 
 
-def compute_deterministic_fraction(empirical_model: EmpiricalModel,
-                                   solver: str = "MOSEK", verbose: bool = True):
-    MS = empirical_model.measurement_scenario
-    X, M, O = MS.X, MS.M, MS.O
-    ve = empirical_model.vector
+def quickhull(POINTS, abs_tol=1e-7):
+    # Little overhead to have matrix independance before
+    _, inds = sympy.Matrix(POINTS).T.rref()
+    return _quickhull(POINTS[inds, :], abs_tol=abs_tol)
 
-    # region NS polytope computation
+
+def NC_polytope(MS: MeasurementScenario) -> np.ndarray:
+    X, M, O = MS.X, MS.M, MS.O
     outcomes_assignements = list(itertools.product([0, 1], repeat=len(X)))
     D = []
     for assignement in outcomes_assignements:
@@ -72,8 +75,78 @@ def compute_deterministic_fraction(empirical_model: EmpiricalModel,
                     d.append(0)
         D.append(d)
 
-    D = np.array(D)
-    # endregion
+    return np.array(D)
+
+
+def compatibility_of_marginals_constraints(MS: MeasurementScenario, hvm: cp.Variable):
+    O, M = MS.O, MS.M
+    outcomes = list(itertools.product(O, repeat=len(M[0])))
+    nb_outcomes = len(outcomes)
+    constraints = []
+    # Compatibility of marginals TODO: improve the loop perf
+    for i, ctx1 in enumerate(M):
+        for j, ctx2 in enumerate(M):
+            if ctx1 == ctx2:
+                continue
+            # Also counting same elements, useless
+            intersection = np.intersect1d(ctx1, ctx2)
+            if intersection.size > 0:
+                # Note the intersection value (is it A0, A1 ...)
+                intersection_value = int(intersection[0])
+
+                # Find the position in the context (if we are looking for A1 in A0A1 and in A1A2 then i_ctx1 = 1 and
+                # j_ctx2 = 0)
+                i_ctx1 = ctx1.index(intersection_value)
+                j_ctx2 = ctx2.index(intersection_value)
+
+                # Note the position of the values to sum
+                ctx_1_indices: List[List[int]] = [[] for _ in range(len(O))]
+                ctx_2_indices: List[List[int]] = [[] for _ in range(len(O))]
+                for k, outcome in enumerate(outcomes):
+                    ctx_1_indices[outcome[i_ctx1]].append(k)
+                    ctx_2_indices[outcome[j_ctx2]].append(k)
+
+                # Finally get the context and add the constraint
+                h_NS_ctx1 = hvm[i * nb_outcomes: (i + 1) * nb_outcomes]
+                h_NS_ctx2 = hvm[j * nb_outcomes: (j + 1) * nb_outcomes]
+
+                for ind1, ind2 in zip(ctx_1_indices, ctx_2_indices):
+                    m_ctx1 = cp.Constant(0)
+                    m_ctx2 = cp.Constant(0)
+                    for ind11, ind21 in zip(ind1, ind2):
+                        m_ctx1 += h_NS_ctx1[ind11]
+                        m_ctx2 += h_NS_ctx2[ind21]
+
+                    constraints += [m_ctx1 == m_ctx2]
+    return constraints
+
+
+def compute_deterministic_fraction(empirical_model: EmpiricalModel,
+                                   solver: str = "MOSEK", verbose: bool = True):
+    """
+    The compute_deterministic_fraction function computes the deterministic fraction of a given empirical model.
+    The function takes as input an EmpiricalModel object and returns the value of its deterministic fraction.
+
+    :param empirical_model:EmpiricalModel: Used to Compute the deterministic fraction.
+    :param solver:str="MOSEK": Used to Specify the solver to be used.
+    :param verbose:bool=True: Used to Display the computation details.
+    :return: The value of the deterministic fraction.
+
+    :doc-author: Trelent
+    """
+    """
+    The compute_deterministic_fraction function computes the deterministic fraction of a given empirical model.
+    The function takes as input an EmpiricalModel object and returns the value of its deterministic fraction.
+    
+    :param empirical_model:EmpiricalModel: Used to Compute the.
+    :param solver:str="MOSEK": Used to Specify the solver to be used.
+    :param verbose:bool=True: Used to Display the computation details.
+    :return: The value of the deterministic fraction.
+    
+    :doc-author: Trelent
+    """
+    ve = empirical_model.vector
+    D = NC_polytope(empirical_model.measurement_scenario)
 
     # Then use linear programming to obtain the deterministic fraction
     c = cp.Variable(D.shape[0], nonneg=True)
@@ -130,41 +203,7 @@ def compute_signaling_fraction(empirical_model: EmpiricalModel,
     for i in range(0, nb_entries, nb_outcomes):
         constraints += [cp.sum(h_NS[i: i + nb_outcomes]) == z]
 
-    # Compatibility of marginals TODO: improve the loop perf
-    for i, ctx1 in enumerate(M):
-        for j, ctx2 in enumerate(M):
-            if ctx1 == ctx2:
-                continue
-            # Also counting same elements, useless
-            intersection = np.intersect1d(ctx1, ctx2)
-            if intersection.size > 0:
-                # Note the intersection value (is it A0, A1 ...)
-                intersection_value = int(intersection[0])
-
-                # Find the position in the context (if we are looking for A1 in A0A1 and in A1A2 then i_ctx1 = 1 and
-                # j_ctx2 = 0)
-                i_ctx1 = ctx1.index(intersection_value)
-                j_ctx2 = ctx2.index(intersection_value)
-
-                # Note the position of the values to sum
-                ctx_1_indices: List[List[int]] = [[] for _ in range(len(O))]
-                ctx_2_indices: List[List[int]] = [[] for _ in range(len(O))]
-                for k, outcome in enumerate(outcomes):
-                    ctx_1_indices[outcome[i_ctx1]].append(k)
-                    ctx_2_indices[outcome[j_ctx2]].append(k)
-
-                # Finally get the context and add the constraint
-                h_NS_ctx1 = h_NS[i * nb_outcomes: (i + 1) * nb_outcomes]
-                h_NS_ctx2 = h_NS[j * nb_outcomes: (j + 1) * nb_outcomes]
-
-                for ind1, ind2 in zip(ctx_1_indices, ctx_2_indices):
-                    m_ctx1 = cp.Constant(0)
-                    m_ctx2 = cp.Constant(0)
-                    for ind11, ind21 in zip(ind1, ind2):
-                        m_ctx1 += h_NS_ctx1[ind11]
-                        m_ctx2 += h_NS_ctx2[ind21]
-
-                    constraints += [m_ctx1 == m_ctx2]
+    constraints += compatibility_of_marginals_constraints(MS, h_NS)
 
     prob = cp.Problem(cp.Minimize(cp.sum(ve - h_NS)), constraints)
     prob.solve(solver=solver, verbose=verbose)
@@ -177,7 +216,6 @@ def compute_signaling_fraction(empirical_model: EmpiricalModel,
 
 def compute_NCF(empirical_model: EmpiricalModel,
                 solver: Optional[str] = 'MOSEK', verbose: bool = True) -> Dict[str, float]:
-
     MS = empirical_model.measurement_scenario
     ve = empirical_model.vector
 
@@ -187,16 +225,68 @@ def compute_NCF(empirical_model: EmpiricalModel,
 
     n = len(outcomes_global)
 
-    b = cp.Variable(n)
+    b = cp.Variable(n, nonneg=True)
 
     incidence_matrix = MS.incidence_matrix
 
     # Define problem and solve it.
-    constraints = [b >= 0]
-
-    constraints += [incidence_matrix @ b <= ve]
+    constraints = [incidence_matrix @ b <= ve]
 
     prob = cp.Problem(cp.Maximize(np.ones(n).T @ b), constraints)
     prob.solve(solver=solver, verbose=verbose)
 
     return {"opt_sol": b.value, "NCF": prob.value, "CF": 1 - prob.value}
+
+
+def compute_max_CF(MS: MeasurementScenario, solver: Optional[str] = "MOSEK",
+                   verbose: Optional[bool] = False) -> Dict[str, Any]:
+    """
+    LP to find the maximum distance between two empirical models. WARNING: it may be long AFAIK.
+
+    :param MS: The measurement scenario in which we try to find the maximum CF.
+    :type MS: MeasurementScenario
+    :param solver: The solver used for the LP. Defaults to 'MOSEK'.
+    :type solver: str
+    :param verbose: Whether the solver should verbose. Defaults to False.
+    :type verbose: bool
+    """
+    # Non-signalling case
+    O, X, M = MS.O, MS.X, MS.M
+    outcomes = list(itertools.product(O, repeat=len(MS.M[0])))
+    nb_outcomes = len(outcomes)
+    nb_contexts = len(MS.M)
+    # NC polytope for h
+    D = NC_polytope(MS)
+    A, b, vert = quickhull(D)
+
+    # b = cp.Variable(n, nonneg=True)
+    z = cp.Variable(1)
+    # Any point in the NS polytope
+    ve = cp.Variable(len(MS.X) * nb_outcomes, nonneg=True)
+
+    # Coefficients for convex combination
+    # x = cp.Variable(D.shape[0], nonneg=True)
+
+    # Any point in the NC polytope
+    # h = cp.Variable(len(MS.X) * nb_outcomes, nonneg=True)
+
+    # Define problem and solve it.
+    constraints = compatibility_of_marginals_constraints(MS, ve)
+
+    # ve is a probability distribution : each row sum to unity
+    for i in range(0, nb_contexts):
+        constraints += [cp.sum(ve[i * nb_outcomes: (i + 1) * nb_outcomes]) == cp.Constant(1)]
+
+    # Convex => sum to one
+    # constraints += [cp.sum(x) == cp.Constant(1)]
+
+    # Distance in LP is not a piece of cake
+    # Works only if highly symmetric
+
+    for h_NC in D:
+        constraints += [z <= cp.sum(h_NC - ve)]
+
+    prob = cp.Problem(cp.Maximize(z), constraints)
+    prob.solve(solver=solver, verbose=verbose)
+
+    return {"EmpiricalModel": EmpiricalModel(MS, ve.value), "distance": z.value}
