151041abc3a1f7f89e4f24bb06a9236ffbdf326f
Kim Vallée <kim.vallee@protonmail.com>
2025-10-30 14:10:55 +0100

    Updating type hinting in the docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -34,7 +34,7 @@ extensions = [
     'sphinx.ext.autodoc'
 ]
 
-autodoc_typehints = 'none'
+autodoc_typehints = 'description'
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
